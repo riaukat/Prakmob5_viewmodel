9634b5a8bc28f3d4cfb8eb8780e0c33dc8ed072b
Pr1 <valeriauka04@gmail.com>
2024-11-21 22:59:55 +0700

    commit

---
+++ app/src/main/java/com/valeriaukat/unscramble/ui/GameUiState.kt
@@ -0,0 +1,9 @@
+package com.valeriaukat.unscramble.ui
+
+data class GameUiState(
+        val currentScrambledWord: String = "",
+        val currentWordCount: Int = 1,
+        val score: Int = 0,
+        val isGuessedWordWrong: Boolean = false,
+        val isGameOver: Boolean = false
+)
